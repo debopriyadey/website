d8c3f471c3b6a9e11888317a793877bcde5fe4db
Debopriya Dey <64556550+debopriyadey@users.noreply.github.com>
2023-05-26 22:10:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("June 1, 2022 15:37:25").getTime();
+var countDownDate = new Date("June 1, 2024 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
